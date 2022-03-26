8f88304b2a1797521708e624686700d808377fdc
Trong-Thang Pham <thang.trong.pham@aioz.io>
2022-03-26 16:42:38 +0700

    add argparse

--- run_.py
+++ run_.py
@@ -1,12 +1,20 @@
+from ast import arg
 from pathlib import Path 
 import os
-
-from torch import mul 
-
-datapath = Path('/media/data/aioz-thang/main_dev/dataset/dataset')
+import argparse
+parser = argparse.ArgumentParser(description='AlphaPose Demo')
+parser.add_argument('--inpdir', dest='inpdir',
+                    help='input-directory', default="inputs/")
+parser.add_argument('--outdir', dest='outdir',
+                    help='output-directory', default="outputs/")
+parser.add_argument('--gpu', type=str, dest='gpu', default="0",
+                    help='choose which cuda device to use by index e.g. 0 (input -1 for cpu only)')
+args = parser.parse_args()
+os.makedirs(args.outdir,exist_ok=True)
+datapath = Path(args.inpdir)
 for vidf in datapath.glob("*"):
     vidp = list(vidf.glob("*"))[0] 
-    os.makedirs(f"./outputs/{vidf.name}", exist_ok=True)
+    os.makedirs(os.path.join(args.outdir,vidf.name), exist_ok=True)
     multiplier = 2
-    os.system(f'python scripts/demo_inference.py --gpus 0 --posebatch {64*multiplier} --detbatch {5*multiplier} --cfg configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-regression.yaml --checkpoint pretrained_models/multi_domain_fast50_regression_256x192.pth --video="{str(vidp)}" --outdir "outputs/{vidf.name}" --save_video --pose_track --sp')
+    os.system(f'python scripts/demo_inference.py --gpus {args.gpu} --posebatch {64*multiplier} --detbatch {5*multiplier} --cfg configs/halpe_coco_wholebody_136/resnet/256x192_res50_lr1e-3_2x-regression.yaml --checkpoint pretrained_models/multi_domain_fast50_regression_256x192.pth --video="{str(vidp)}" --outdir "{os.path.join(args.outdir,vidf.name)}" --save_video --pose_track --sp')
     # break
